160a48316a6e2d467725cc2804584ee9dfc6b9eb
Your Name <you@example.com>
2023-12-29 20:56:23 +0000

    created JS script to log the total months in the dataset

--- starter/index.js
+++ starter/index.js
@@ -86,3 +86,17 @@ var finances = [
   ['Jan-2017', 138230],
   ['Feb-2017', 671099],
 ];
+
+// Number of months included in the dataset
+var months = (finances.length);{
+  console.log("Total months: " + months);
+}
+// The net total amount of Profit/Losses over the entire period
+
+// * The average of the **changes** in Profit/Losses over the entire period.
+//   * You will need to track what the total change in Profit/Losses are from month to month and then find the average.
+//   * (`Total/(Number of months - 1)`)
+
+// * The greatest increase in Profit/Losses (date and difference in the amounts) over the entire period.
+
+// * The greatest decrease in Profit/Losses (date and difference in the amounts) over the entire period.
